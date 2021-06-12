be64f3081cdc152ede5fc1d8b0cb30f4f7b8f7d6
Tristin <twatson5543@gmail.com>
2021-06-12 19:05:19 -0400

    Removed some comments and redundant Functions

--- PowerFactor.py
+++ PowerFactor.py
@@ -1,3 +1,4 @@
+# === Libraries ===
 import math
 
 # ==== Values ====
@@ -6,11 +7,6 @@ hz = int(input("Type Hz:              "))
 resistance = int(input("Type Resistor Value:  "))
 inductance = float(input("Type Inductor Value:  "))
 
-#
-#
-#
-#
-#
 # ==== Functions ====
 def Xl(l):
     XofL = 2 * math.pi * hz * l
@@ -27,99 +23,93 @@ def ZtotalAng_1(Xl):
     imag_final = math.degrees(imag_2)
     return imag_final
 
-def GetCurrent(zTotal):
-    Current = Voltage / zTotal
-    return Current
-
-def GetCurrentAngle(Zdeg):
-    CurrentDeg = -Zdeg
-    return CurrentDeg
-
+# Get True Power from Current and Resistance
 def GetTruePower(It):
     TruePower = ((It) * (It)) * resistance
     return TruePower
 
+# Get True Power from Apparent Power * Cosine of Magnitude Angle
 def GetTruePower2(S,zAng):
     TruePower = (S) * (math.cos(math.radians(zAng)))
     return TruePower
 
+# Get Apparent Power
 def GetApparentPower(Zt,It):
     ApparentPower = ((It)*(It)*Zt)
     return ApparentPower
 
+# Get Reactive Power
 def GetReactPower(S,zAng):
     ReactivePower = (S) * (math.sin(math.radians(zAng)))
     return ReactivePower
 
-def Xsolve(Q):
-    X = (Voltage * Voltage) / Q
-    return X
-
-def Csolve(Xc):
-    C = 1 / (2 * math.pi * hz * Xc)
-    return C
-
-def XofCsolve(Cval):
-    XoC = 1 / (2 * math.pi * hz * Cval)
-    return XoC
-
+# Get Real Number from Polar Form
 def GetRealNum(realVal,realAng):
     realMag = float((math.cos(math.radians(realAng))))
     realMag2 = round(realMag,12)
     realMag3 = realMag2 * realVal
     return realMag3
 
+# Get Imaginary Number from Polar Form
 def GetImagNum(realVal,realAng):
     ImagNum1 = float((math.sin(math.radians(realAng))))
     ImagNum2 = round(ImagNum1,12)
     ImagNum3 = ImagNum2 * realVal
     return ImagNum3
 
+# Get X of C from Reactive Power
+def Xsolve(Q):
+    X = (Voltage * Voltage) / Q
+    return X
+
+# Get X of C
+def XofCsolve(Cval):
+    XoC = 1 / (2 * math.pi * hz * Cval)
+    return XoC
+
+# Get Magnitude of Squared Form
 def GetMag(realVal,ImagVal):
     real_1 = ((ImagVal) * (ImagVal)) + ((realVal) * (realVal))
     real_final = math.sqrt(real_1)
     return real_final
 
+# Get Angular of Squared Form
 def GetAng(realVal, ImagVal):
     imag_1 = ImagVal / realVal
     imag_2 = math.atan(imag_1)
     imag_final = math.degrees(imag_2)
     return imag_final
-#
-#
-#
-#
-#
+
 # ==== Math Steps ====
 
-# >> Get Xl
+# Get Xl
 XofL = Xl(inductance)
 
-# >> Get Total Z
+# Get Total Z
 zTotalReal_1 = ZtotalMag_1(XofL)
 zTotalDeg_1 = ZtotalAng_1(XofL)
 
-# >> Get Current
-UnCorI = GetCurrent(zTotalReal_1)
-UnCorIang = GetCurrentAngle(zTotalDeg_1)
+# Get Current
+UnCorI = Voltage / zTotalReal_1
+UnCorIang = -1 * (zTotalDeg_1)
 
-# >> Real Power
+# Real Power
 UnCorTruePower = GetTruePower(UnCorI)
 
-# >> Apparent Power
+# Apparent Power
 UnCorAppPower = GetApparentPower(Zt=zTotalReal_1, It=UnCorI)
 
-# >> Reactive Power
+# Reactive Power
 UnCorReactPower = GetReactPower(S=UnCorAppPower, zAng=zTotalDeg_1)
 
-# >> Solve for X
+# Solve for X
 XofC = Xsolve(UnCorReactPower)
 
-# >> Solve for C
-Cvalue = Csolve(XofC)
+# Solve for C
+Cvalue = XofCsolve(XofC)
 tCvalue = 0.000022 # <-------- Temporary Value
 
-# >> Find Z Total
+# Find Z Total
 ZrCorrect = resistance
 ZrAng = float(0)
 ZlCorrect = XofL
@@ -131,91 +121,60 @@ ZcAng = float(-90)
 NumRealSum = GetRealNum(realVal=ZrCorrect, realAng=ZrAng) + GetRealNum(realVal=ZlCorrect, realAng=ZlAng) + GetRealNum(realVal=ZcCorrect, realAng=ZcAng)
 NumImagSum = GetImagNum(realVal=ZrCorrect, realAng=ZrAng) + GetImagNum(realVal=ZlCorrect, realAng=ZlAng) + GetImagNum(realVal=ZcCorrect, realAng=ZcAng)
 
-#print(NumRealSum)
-#print(NumImagSum)
-
 DenRealSum1 = GetRealNum(realVal=ZrCorrect, realAng=ZrAng) + GetRealNum(realVal=ZlCorrect, realAng=ZlAng)
 DenImagSum1 = GetImagNum(realVal=ZrCorrect, realAng=ZrAng) + GetImagNum(realVal=ZlCorrect, realAng=ZlAng)
-
-#print(DenRealSum1)
-#print(DenImagSum1)
 
 DenMag1 = GetMag(realVal=DenRealSum1, ImagVal=DenImagSum1)
 DenAng1 = GetAng(realVal=DenRealSum1, ImagVal=DenImagSum1)
 
-#print("Den:",DenMag1)
-#print("Den:",DenAng1)
-
 NumMag1 = GetMag(realVal=NumRealSum, ImagVal=NumImagSum)
 NumAng1 = GetAng(realVal=NumRealSum, ImagVal=NumImagSum)
-
-#print("Num:",NumMag1)
-#print("Num:",NumAng1)
 
 DenMultiMag = DenMag1 * ZcCorrect
 DenMultiAng = DenAng1 + ZcAng
 
-#print("Den:",DenMultiMag)
-#print("Den:",DenMultiAng)
-
-
 ZtotalMag1 = NumMag1 / DenMultiMag
 ZtotalAng1 = NumAng1 - DenMultiAng
-
-#print(ZtotalMag1)
-#print(ZtotalAng1)
 
 ZtotalMagCorrect = 1 / ZtotalMag1
 ZtotalAngCorrect = -1 * ZtotalAng1
 
-#print("Ztotal Mag:",ZtotalMagCorrect)
-#print("Ztotal Ang:",ZtotalAngCorrect)
-
-# >>> Find New Total Current
-
+# Find New Total Current
 CorrectI = Voltage / ZtotalMagCorrect
 CorrectIang = ZtotalAngCorrect * -1
 
-# >>> Find New Apparent Power
-
+# Find New Apparent Power
 CorrectAppPower = GetApparentPower(Zt=ZtotalMagCorrect, It=CorrectI)
 
-# >>> Find New True Power
-
+# Find New True Power
 CorrectTruePower = GetTruePower2(CorrectAppPower,ZtotalAngCorrect)
 
-# >>> Find New Reactive Power
-
+# Find New Reactive Power
 CorrectReactPower = GetReactPower(CorrectAppPower,ZtotalAngCorrect)
 
-# >>> Find Power Factor
-
+# Find Power Factor
 PowerFactor = CorrectTruePower / CorrectAppPower
 
-#
-#
-#
-#
-#
-# ==== Proof ====
-
+# ==== Printout ====
 
 # -- Uncorrected Values --
 print("")
 print("Uncorrected Values:")
 print("Xl = ",str(XofL) + "Ω∡-90∘")
 print("Z Total =", str(zTotalReal_1) + "Ω∡" + str(zTotalDeg_1) + "∘")
-print("I Total =",str(UnCorI) + "Ω∡" + str(UnCorIang) + "∘")
+print("I Total =",str(UnCorI) + "∡" + str(UnCorIang) + "∘")
 print("True Power (P):", UnCorTruePower)
 print("Apparent Power (S):", UnCorAppPower)
 print("Reactive Power (Q):",UnCorReactPower)
 
+# -- Capacitor Values --
 print("")
 print("Corrective Capacitance:")
 print("Xc =", str(XofC) + "∡-90∘")
 print("C =", str(Cvalue)+"F")
 print("Capacitor Value =", str((Cvalue * math.pow(10, 6))) + "μF")
 
+# -- Corrected Values --
 print("")
 print("Corrected Values:")
 print("Z Total =", str(ZtotalMagCorrect) + "∡" + str(ZtotalAngCorrect) + "∘")
@@ -224,14 +183,6 @@ print("True Power (P):", CorrectTruePower)
 print("Apparent Power (S):", CorrectAppPower)
 print("Reactive Power (Q):", CorrectReactPower)
 
+# -- Power Factor --
 print("")
 print("Power Factor:",PowerFactor)
-
-"""
-val1 = (GetRealNum(realVal=ZrCorrect,realAng=ZrAng))
-val2 = GetRealNum(realVal=ZlCorrect, realAng=ZlAng)
-val3 = GetRealNum(realVal=ZcCorrect, realAng=ZcAng)
-print(val1)
-print(val2)
-print(val3)
-"""
